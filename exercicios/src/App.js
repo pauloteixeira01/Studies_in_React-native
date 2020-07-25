8b73696a2099ec18108bf84aa1bf1623f1407329
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-07-25 16:25:00 -0300

    Gerando números aleatórios

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import Aleatorio from './components/Aleatorio'
+
 
 import MinMax from './components/MinMax';
 // import CompPadrao, { Comp1, Comp2 } from './components/Multi';
@@ -7,18 +9,24 @@ import MinMax from './components/MinMax';
 
 export default () => (
     <View style={style.App}>
-        <MinMax min="3" max="20"/>
+        <Aleatorio min={0} max={10}/>
+        <Aleatorio min={0} max={10}/>
+        <Aleatorio min={0} max={10}/>
+        <Aleatorio min={0} max={10}/>
+       
+        {/*<MinMax min="3" max="20"/>
         <MinMax min="1" max="94"/>
-        {/*<CompPadrao />
-            <Comp1 />
-            <Comp2 />
-            <Primeiro />*/}    
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />*/}    
     </View>
 )
 
 const  style = StyleSheet.create({
     App: {
         //backgroundColor:'#F00',
+        color: '#F00',
         flexGrow:1,
         justifyContent:"center",
         alignItems: "center",
---
+++ exercicios/src/components/Aleatorio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Estilo from './estilo';
+
+export default props => {
+    const {min, max} = props; // destructure
+    const aleatorio = parseInt(Math.random() * (max - min) + min);
+    return (
+        <Text style={Estilo.txtG}>
+            O valor aleatório é: {aleatorio}
+        </Text>
+    )
+}
