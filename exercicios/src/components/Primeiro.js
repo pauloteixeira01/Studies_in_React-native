ed05506e02c79119cc0e4b417a5b6560330edd81
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-07-25 10:49:38 -0300

    console.warn() é a mesma coisa do console.log()

--- exercicios/src/components/Primeiro.js
+++ exercicios/src/components/Primeiro.js
@@ -3,3 +3,10 @@ import { Text } from 'react-native';
 import Estilo from './estilo';
 
 export default () => <Text style={Estilo.fontG}>Primeiro!</Text>
+
+{
+    /*
+    console.warn()
+    Serve pra debugar. Mesma coisa do console.log() 
+    */
+}
