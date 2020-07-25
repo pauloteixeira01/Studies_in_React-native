e2ef1b615a90704cf8d45a44baaaa05a00bd490b
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-07-25 11:22:47 -0300

    Componente com propriedades(parâmetros)

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Primeiro from './components/Primeiro';
-import X, { Comp1, Comp2 } from './components/Multi';
+
+import MinMax from './components/MinMax';
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi';
+// import Primeiro from './components/Primeiro';
 
 export default () => (
     <View style={style.App}>
-        <X />
-        <Comp1 />
-        <Comp2 />
-        <Primeiro />
+        <MinMax min="3" max="20"/>
+        <MinMax min="1" max="94"/>
+        {/*<CompPadrao />
+            <Comp1 />
+            <Comp2 />
+            <Primeiro />*/}    
     </View>
 )
 
@@ -17,6 +21,7 @@ const  style = StyleSheet.create({
         //backgroundColor:'#F00',
         flexGrow:1,
         justifyContent:"center",
-        alignItems: "center"
+        alignItems: "center",
+        padding: 20
         }
 })
--- exercicios/src/components/Primeiro.js
+++ exercicios/src/components/Primeiro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 import Estilo from './estilo';
 
-export default () => <Text style={Estilo.fontG}>Primeiro!</Text>
+export default () => <Text style={Estilo.txtG}>Primeiro!</Text>
 
 {
     /*
