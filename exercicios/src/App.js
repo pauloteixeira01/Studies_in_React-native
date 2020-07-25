bf24080291a811a5346a7a6afc5ae8096a0f199c
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-07-25 10:41:13 -0300

    Estilização

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Primeiro from './components/Primeiro';
 import X, { Comp1, Comp2 } from './components/Multi';
 
 export default () => (
-    <View>
+    <View style={style.App}>
         <X />
         <Comp1 />
         <Comp2 />
         <Primeiro />
     </View>
 )
+
+const  style = StyleSheet.create({
+    App: {
+        //backgroundColor:'#F00',
+        flexGrow:1,
+        justifyContent:"center",
+        alignItems: "center"
+        }
+})
